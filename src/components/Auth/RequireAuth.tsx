11f9384255d82938dfc6e86f03e681e078db910f
ssani7 <sanaullah.sani756@gmail.com>
2024-06-12 02:23:16 +0600

    added courses

--- src/components/Auth/RequireAuth.tsx
+++ src/components/Auth/RequireAuth.tsx
@@ -3,9 +3,26 @@
 import { useUserContext } from '@/contexts/UserContext';
 import { requestAuthorization } from '@/utils/auth';
 import { UserData } from '@/utils/types/GeneralTypes';
-import { Backdrop, Button } from '@mui/material';
+import { Backdrop, Box, Button, Typography } from '@mui/material';
 import React, { ReactNode } from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
+
+const style = {
+	position: 'absolute' as 'absolute',
+	top: '50%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
+	width: 400,
+	// bgcolor: 'background.',
+	border: '2px solid #000',
+	boxShadow: 24,
+	p: 4,
+	display: 'flex',
+	flexDirection: 'column',
+	gap: '18px',
+	alignItems: 'center',
+	justifyContent: 'center',
+};
 
 const RequireAuth = ({ children }: { children: ReactNode }) => {
 	const { userData, loadingUser } = useUserContext();
@@ -25,7 +42,16 @@ const RequireAuth = ({ children }: { children: ReactNode }) => {
 	else
 		return (
 			<div>
-				<Button onClick={requestAuthorization}>Log In</Button>
+				<Box sx={style}>
+					<Typography id="modal-modal-title" variant="h6" component="h2">
+						Please Log In To continue
+					</Typography>
+					<div>
+						<Button variant="contained" onClick={requestAuthorization}>
+							Log In
+						</Button>
+					</div>
+				</Box>
 			</div>
 		);
 };
